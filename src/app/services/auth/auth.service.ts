9893cbacf9c7fa36ab496eca05206c06e1d67bd7
Arnav Puri <arnavpuri08@gmail.com>
2025-04-04 15:09:28 +0530

    admin_login

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
+import { UserStorageService } from '../storage/user-storage.service';
 
 const BASIC_URL = 'http://localhost:8081/';
 
@@ -8,9 +9,36 @@ const BASIC_URL = 'http://localhost:8081/';
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private userStorageService: UserStorageService
+  ) {}
 
   register(signupRequest: any): Observable<any> {
     return this.http.post(BASIC_URL + 'sign-up', signupRequest);
+  }
+
+  login(username: string, password: string): Observable<boolean> {
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    const body = { username, password };
+
+    return this.http
+      .post(BASIC_URL + 'authenticate', body, { headers, observe: 'response' })
+      .pipe(
+        map((res) => {
+          const authHeader = res.headers.get('authorization');
+          const token = authHeader?.startsWith('Bearer ')
+            ? authHeader.substring(7)
+            : null;
+          const user = res.body;
+
+          if (token && user) {
+            this.userStorageService.saveToken(token);
+            this.userStorageService.saveUser(JSON.stringify(user));
+            return true;
+          }
+          return false;
+        })
+      );
   }
 }
